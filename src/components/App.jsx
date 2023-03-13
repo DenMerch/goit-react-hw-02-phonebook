b2855cebdd7ac3a66fb4e96029bacf789429211a
DenMerch <rosignolli@gmail.com>
2023-03-13 08:32:15 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,10 @@ import { Contacts } from "./Contacts/Contacts";
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },],
     name: '',
     number: '',
     filter: '',
@@ -33,18 +36,9 @@ export class App extends Component {
     )
 
   }
-  // foundedName = (name, contacts) => {
-  //   const newContacts = contacts.filter(contact => {
-
-  //     if (contact.name.toLowerCase().replace(" ", '').includes(name)) {
-  //       return contact
-  //     }
-
-
-
-  //   })
-  //   return newContacts;
-  // }
+  foundedName = (name, contacts) => {
+    return contacts.filter(contact => contact.name.toLowerCase().replace(" ", '').includes(name))
+  }
   render() {
     return (
       <div
